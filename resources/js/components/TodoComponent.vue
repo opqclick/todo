a2af4b1bb7551c71cf4d5061f9b43ed08e91587a
Md Abul Hasnat <opqclick@gmail.com>
2023-07-25 16:40:37 +0600

    staging first step

--- resources/js/components/TodoComponent.vue
+++ resources/js/components/TodoComponent.vue
@@ -9,20 +9,21 @@
                       <div class="inputs-area">
                         <div class="mb-3">
                           <label for="title" class="form-label">Title</label>
-                          <input type="text" class="form-control" id="title" aria-describedby="title">
+                          <input type="text" class="form-control" id="title" aria-describedby="title" v-model="input_title">
                         </div>
                         <div class="mb-3">
                           <label for="description" class="form-label">Description</label>
-                          <textarea class="form-control" placeholder="Leave a description here" id="description" style="height: 100px"></textarea>
+                          <textarea class="form-control" placeholder="Leave a description here" id="description" style="height: 100px" v-model="input_description"></textarea>
                         </div>
                         <div class="mb-3">
                           <label for="deadline" class="form-label">Deadline</label>
-                          <input class="form-control" type="datetime-local" id="deadline">
+                          <input class="form-control" type="datetime-local" id="deadline" v-model="input_deadline">
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                       </div>
                     </div>
                 </div>
+
               <div class="card mt-2">
                 <div class="card-header">Todos </div>
                 <div class="card-body">
@@ -37,11 +38,11 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>Otto</td>
+                    <tr v-for="(todo, index) in todos" :key="index">
+                      <th scope="row">{{ ++index }}</th>
+                      <td>{{ todo.title }}</td>
+                      <td>{{ todo.description.slice(0, 50) }}</td>
+                      <td>{{ todo.deadline }}</td>
                       <td class="text-center">
                         <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
                         <button type="button" class="btn btn-sm  btn-outline-success">Complete</button>
@@ -67,11 +68,22 @@
 </template>
 
 <script>
-    import DatepickerComponent from "./DatepickerComponent.vue";
     export default {
-      components: {DatepickerComponent},
-      mounted() {
-            console.log('Component mounted.')
+      data(){
+        return{
+          todos:[],
+          api: 'https://tt-todo.test/api/todos',
+          input_title: '',
+          input_description: '',
+          input_deadline: '',
+
         }
+      },
+      mounted() {
+        // Get api data
+       this.axios.get(this.api).then(res=>{
+         this.todos = res.data;
+       })
+      }
     }
 </script>
